9202d9f9b1fa792e7474103b5f23e5a931f8d1b4
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-04-05 23:07:30 +0300

    Add DataManager

---
+++ SpringMyApp/Model.swift
@@ -0,0 +1,22 @@
+//
+//  Model.swift
+//  SpringMyApp
+//
+//  Created by Alexandr Barabash on 05.04.2022.
+//
+
+import Foundation
+
+struct Animation {
+    
+    let preset: String
+    let curve: String
+    let forse: Double
+    let duration: Double
+    let delay: Double
+    
+    
+   
+    
+    enum RepeatMode
+}
--- SpringMyApp/ViewController.swift
+++ SpringMyApp/ViewController.swift
@@ -5,15 +5,20 @@
 //  Created by Alexandr Barabash on 05.04.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var springAnimationView: SpringView!
+    @IBOutlet var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func runSpringAnimation(_ sender: SpringButton) {
+    }
+    
 }
 
